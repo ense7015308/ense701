7d7e9468b6790baa7537b49eae89d91de24e730b
jasmimi <jasmine_amohia@hotmail.com>
2023-10-14 02:41:45 +1300

    Commented and tidied code to adhere to group code standards

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,29 @@
+/* 
+  name: index.tsx
+  desc: home page. search entry field and button returns included titles records
+*/
+
+// imports
 import React, { useState } from "react";
 import { Article, fetchArticles } from "../pages/articles/index";
 import styles from "./home.module.scss";
 
-// Define a type for sortable columns
+// defines headings for sortable columns to sort by
 type SortableColumns = "title" | "authors" | "journname" | "pubyear" | "volume" | "num" | "pages" | "doi";
 
+// export
 export default function Home() {
+
+  // initialise constants
+  // search-related constants
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [matchingArticles, setMatchingArticles] = useState<Article[]>([]);
+
+  // sorting-related constants
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
   const [sortedColumn, setSortedColumn] = useState<SortableColumns | null>(null);
 
+  // function to accept title-search input, find matching articles, and return them
   const handleSearch = async () => {
     try {
       const articles = await fetchArticles();
@@ -18,47 +31,65 @@ export default function Home() {
         return article.title.toLowerCase().includes(searchTerm.toLowerCase());
       });
       setMatchingArticles(matchingArticles);
+
+    // error handling
     } catch (error) {
       console.error("Error fetching articles: ", error);
     }
   };
 
+  // function to sort table data by column selection
   const handleSort = (column: SortableColumns) => {
+
+    // choose which column name and sort type
     const isAsc = sortedColumn === column && sortOrder === "asc";
     setSortedColumn(column);
     setSortOrder(isAsc ? "desc" : "asc");
   
+    // sortable articles are records that the search returnd
     const sortedArticles = [...matchingArticles];
     sortedArticles.sort((a, b) => {
       const aValue = a[column];
       const bValue = b[column];
       
+      // 'authors' column is array object, so the sorting algorithm is different
       if (column === "authors") {
         const aAuthorsString = a.authors.join(", ");
         const bAuthorsString = b.authors.join(", ");
-
         if (isAsc) {
           return aAuthorsString.localeCompare(bAuthorsString);
         } else {
           return bAuthorsString.localeCompare(aAuthorsString);
         }
+
+      // 'title', 'journal name', 'pages', and 'doi' columns have atomic string values
       } else if (typeof aValue === "string" && typeof bValue === "string") {
         return isAsc ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+      
+      // 'publish year', 'volume', and 'num' columns have atomic number values
       } else if (typeof aValue === "number" && typeof bValue === "number") {
         return isAsc ? aValue - bValue : bValue - aValue;
+      
+      // error handling
       } else {
         return 0;
       }
     });
   
+    // set final matching articles to those that match the search input, with user-selected sorting
     setMatchingArticles(sortedArticles);
   };
 
+  // returm
   return (
     <div className={styles.container}>
+
+      {/* heading */}
       <h1 className={styles.heading}>
         Software Practice Empirical Evidence Database (SPEED)
       </h1>
+
+      {/* search input field and button */}
       <div className={styles.searchContainer}>
         <input
           type="text"
@@ -71,10 +102,14 @@ export default function Home() {
           Search
         </button>
       </div>
+
+      {/* search results table */}
       <div id="searchResults" className={styles.resultsContainer}>
         <h3>Searching result:</h3>
         {matchingArticles.length > 0 ? (
           <table className={styles.resultsTable}>
+
+            {/* sortable column headings */}
             <thead>
               <tr>
                 <th onClick={() => handleSort("title")}>Title</th>
@@ -87,6 +122,8 @@ export default function Home() {
                 <th onClick={() => handleSort("doi")}>DOI</th>
               </tr>
             </thead>
+
+            {/* matching article values */}
             <tbody>
               {matchingArticles.map((article) => (
                 <tr key={article.id}>
@@ -102,6 +139,8 @@ export default function Home() {
               ))}
             </tbody>
           </table>
+
+        // error handling if there are no matches to the search
         ) : (
           <div>No matching articles found</div>
         )}
