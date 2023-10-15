7744b7c1e6e332120f583a7d156701d150f18fb0
Yue LI <yueli.7888@gmail.com>
2023-10-16 12:16:41 +1300

    Add safeRegex and escapeRegExp to treat user input like .;! as characters instead of regular expression

--- backend/routes/api/search.js
+++ backend/routes/api/search.js
@@ -2,11 +2,23 @@
 const express = require("express");
 const router = express.Router(); // Initialize a new router instance
 const Article = require("../models/Article"); // Import the Article model
+// Import the safe-regex library to ensure the regex we generate is safe from ReDoS attacks.
+const safeRegex = require("safe-regex");
+// Import the escapeRegExp function from lodash to sanitize user input for regular expressions.
+// This will escape any characters that have special meaning in regex, ensuring they're treated as literals.
+const { escapeRegExp } = require('lodash');
 
 // Define a POST route for searching articles based on a keyword
 router.post("/", async (req, res) => {
   // Extract the keyword from the request body
   const userInput = req.body.keyword;
+  // Validate the regex for safety
+  if (!safeRegex(userInput)) {
+    return res.status(400).json({ error: "Invalid search keyword." });
+  }
+
+  // Sanitize the userInput to escape special regex characters
+  const sanitizedInput = escapeRegExp(userInput);
 
   try {
     // Perform a case-insensitive search in the Article collection based on the user input
